ecb67bcb59152435e23f15d96d0dab08bf208eb7
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-18 15:45:57 +0330

    estalbish postgresql connection and set its configuration

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v11"
+	"sync"
+)
+
+var (
+	instance *Config
+	once     sync.Once
+	iniErr   error
+)
+
+type Config struct {
+	Server      Server
+	Postgresql  Postgresql
+	Application Application
+}
+
+func NewConfig() (*Config, error) {
+	once.Do(func() {
+		instance = &Config{}
+		iniErr = env.Parse(instance)
+		if iniErr != nil {
+			instance = nil
+		}
+	})
+	return instance, iniErr
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/saleh-ghazimoradi/FilmFetch
 go 1.25.0
 
 require (
+	github.com/caarlos0/env/v11 v11.3.1
+	github.com/lib/pq v1.10.9
+	github.com/spf13/cobra v1.10.1
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.10.1 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 )
