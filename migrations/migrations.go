1a1932b55a8f53718453b6e68c502852dc6934e2
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-18 16:50:33 +0330

    import the missed package

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
