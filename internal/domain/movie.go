6d5f103edd3df01b908dd3949ea0fed4c3a128be
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-19 20:15:19 +0330

    add movie model to the project

---
+++ internal/domain/movie.go
@@ -0,0 +1,13 @@
+package domain
+
+import "time"
+
+type Movie struct {
+	Id        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitzero"`
+	Runtime   int32     `json:"runtime,omitzero"`
+	Genres    []string  `json:"genres,omitzero"`
+	Version   int32     `json:"version"`
+}
