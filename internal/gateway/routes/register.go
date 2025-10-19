cef1c0517e2681d29f846e29669056eb00d42c98
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-19 20:21:44 +0330

    make use of routes in my register route function

---
+++ internal/gateway/routes/register.go
@@ -0,0 +1,60 @@
+package routes
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/saleh-ghazimoradi/FilmFetch/internal/helper"
+	"github.com/saleh-ghazimoradi/FilmFetch/internal/middleware"
+	"net/http"
+)
+
+type Register struct {
+	customError  *helper.CustomError
+	middleware   *middleware.Middleware
+	healthRoutes *HealthRoutes
+	movieRoutes  *MovieRoutes
+}
+
+type Options func(*Register)
+
+func WithCustomError(customError *helper.CustomError) Options {
+	return func(r *Register) {
+		r.customError = customError
+	}
+}
+
+func WithMiddleware(middleware *middleware.Middleware) Options {
+	return func(r *Register) {
+		r.middleware = middleware
+	}
+}
+
+func WithHealthRoutes(healthRoutes *HealthRoutes) Options {
+	return func(r *Register) {
+		r.healthRoutes = healthRoutes
+	}
+}
+
+func WithMovieRoutes(movieRoutes *MovieRoutes) Options {
+	return func(r *Register) {
+		r.movieRoutes = movieRoutes
+	}
+}
+
+func (r *Register) RegisterRoutes() http.Handler {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(r.customError.NotFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(r.customError.MethodNotAllowedResponse)
+
+	r.healthRoutes.HealthRoute(router)
+	r.movieRoutes.MovieRoute(router)
+
+	return r.middleware.RecoverPanic(router)
+}
+
+func NewRegister(opts ...Options) *Register {
+	r := &Register{}
+	for _, opt := range opts {
+		opt(r)
+	}
+	return r
+}
