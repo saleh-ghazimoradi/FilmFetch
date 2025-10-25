6ab2a3de55ed225c4acbba06cd9b4f060ede52b1
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-25 17:30:39 +0330

    define a user model with some password functionalities

---
+++ internal/domain/user.go
@@ -0,0 +1,44 @@
+package domain
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type User struct {
+	Id        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"version"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	if err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword)); err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
